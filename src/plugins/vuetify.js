bc72f986a96497e17ebc53b6a176680f568eb3d0
wmy <972537949@qq.com>
2023-03-23 09:31:41 +0800

    update

--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -4,17 +4,17 @@ import Vuetify from 'vuetify/lib/framework';
 Vue.use(Vuetify);
 
 export default new Vuetify({
-  theme: {
-    themes: {
-      light: {
-        // primary: '#F57C00',
-        // secondary: '#FB8C00',
-        // accent: '#B39DDB',
-        // info: '#2196F3',
-        // error: '#FF5252',
-        // success: '#4CAF50',
-        // warning: '#FFC107',
-      }
-    }
-  }
+  // theme: {
+  //   themes: {
+  //     light: {
+  //       primary: '#F57C00',
+  //       secondary: '#FB8C00',
+  //       accent: '#B39DDB',
+  //       info: '#2196F3',
+  //       error: '#FF5252',
+  //       success: '#4CAF50',
+  //       warning: '#FFC107',
+  //     }
+  //   }
+  // }
 });
