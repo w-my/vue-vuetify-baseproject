af688ec8b6980814f4ad810de220e42c956ce348
wmy <972537949@qq.com>
2023-03-24 17:53:54 +0800

    update

--- src/components/pages/VuetifyUI.vue
+++ src/components/pages/VuetifyUI.vue
@@ -4,32 +4,32 @@
       <v-expansion-panel>
         <v-expansion-panel-header>v-btn</v-expansion-panel-header>
         <v-expansion-panel-content>
-            <div class="my-2">
-              <v-btn class="btn-primary">Primary</v-btn>
-              <v-btn class="btn-secondary">Secondary</v-btn>
-              <v-btn class="btn-accent">Accent</v-btn>
-              <v-btn class="btn-info">Info</v-btn>
-              <v-btn class="btn-error">Error</v-btn>
-              <v-btn class="btn-success">Success</v-btn>
-              <v-btn class="btn-warning">Warning</v-btn>
-            </div>
-            <div class="my-2">
-              <v-btn text class="c-primary">Primary</v-btn>
-              <v-btn text class="c-secondary">Secondary</v-btn>
-              <v-btn text class="c-accent">Accent</v-btn>
-              <v-btn text class="c-info">Info</v-btn>
-              <v-btn text class="c-error">Error</v-btn>
-              <v-btn text class="c-success">Success</v-btn>
-              <v-btn text class="c-warning">Warning</v-btn>
-            </div>
+          <div class="my-2">
+            <v-btn class="btn-primary">Primary</v-btn>
+            <v-btn class="btn-secondary">Secondary</v-btn>
+            <v-btn class="btn-accent">Accent</v-btn>
+            <v-btn class="btn-info">Info</v-btn>
+            <v-btn class="btn-error">Error</v-btn>
+            <v-btn class="btn-success">Success</v-btn>
+            <v-btn class="btn-warning">Warning</v-btn>
+          </div>
+          <div class="my-2">
+            <v-btn text class="c-primary">Primary</v-btn>
+            <v-btn text class="c-secondary">Secondary</v-btn>
+            <v-btn text class="c-accent">Accent</v-btn>
+            <v-btn text class="c-info">Info</v-btn>
+            <v-btn text class="c-error">Error</v-btn>
+            <v-btn text class="c-success">Success</v-btn>
+            <v-btn text class="c-warning">Warning</v-btn>
+          </div>
         </v-expansion-panel-content>
       </v-expansion-panel>
       <v-expansion-panel>
         <v-expansion-panel-header>v-btn</v-expansion-panel-header>
         <v-expansion-panel-content>
-            <div class="my-2">
-              
-            </div>
+          <div class="my-2">
+            <v-btn @click="loadData">load data</v-btn>
+          </div>
         </v-expansion-panel-content>
       </v-expansion-panel>
     </v-expansion-panels>
@@ -41,6 +41,18 @@ export default {
   name: 'VuetifyUI',
   mounted() {
     console.log("==> id title: ", this.$route.params.id, this.$route.params.title);
+  },
+  methods: {
+    loadData() {
+      this.$axios.get('http://loaclhost:8080/students').then(
+        response => {
+          console.log('请求成功了：', response.data)
+        },
+        error => {
+          console.log('请求失败了：', error.message)
+        }
+      )
+    }
   },
 }
 </script>
--- src/main.js
+++ src/main.js
@@ -3,10 +3,11 @@ import App from './App.vue'
 import vuetify from './plugins/vuetify'
 import VueRouter from 'vue-router'
 import router from './router'
+import axios from 'axios'
 import '@/assets/css/base.css'
 
 Vue.use(VueRouter)
-
+Vue.prototype.$axios = axios;
 Vue.config.productionTip = false
 
 new Vue({
